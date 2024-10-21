253308c6cf27d0d62bf4341b26472e320a2e3c5a
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-10-21 11:04:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    const palpites = document.querySelector('.palpites');
+    const ultimoResultado = document.querySelector('.ultimoResultado');
+    const baixoOuAlto = document.querySelector('.baixoOuAlto');
+    const envioPalpite = document.querySelector('.envioPalpite');
+    const campoPalpite = document.querySelector('.campoPalpite');
+    let contagemPalpites = 1;
+    let botaoReiniciar;
